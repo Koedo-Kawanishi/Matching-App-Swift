bfe1961b3641ae75dc50d326188972d3d6ae8542
nshhhin <shinn070110@gmail.com>
2019-10-06 10:09:52 +0900

    :sparkles: メッセージリストを実装

--- Matching-App-Swift/ViewController/Turtorial/Message/MessageListVC.swift
+++ Matching-App-Swift/ViewController/Turtorial/Message/MessageListVC.swift
@@ -7,9 +7,7 @@ class MessageListVC: UIViewController {
         didSet {
             messageTableV.delegate = self
             messageTableV.dataSource = self
-            messageTableV.register(cellType: MessageCell.self)
-            messageTableV.backgroundColor = .white
-        }
+            messageTableV.register(cellType: MessageCell.self)        }
     }
 }
 
@@ -24,11 +22,17 @@ extension MessageListVC: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(with: MessageCell.self, for: indexPath)
+        cell.selectionStyle = .none
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
+        // 選択不可にする
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let nextVC = self.storyboard?.instantiateViewController(withIdentifier: "MessageVC") as! MessageVC
+        present( nextVC, animated: true )
     }
     
     
